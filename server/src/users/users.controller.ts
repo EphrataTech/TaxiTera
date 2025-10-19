ebb7574f17dd893ae2371a26b4cba4c194a86ea1
Ephrata <ephrata0327@gmail.com>
2025-10-19 13:15:49 +0300

    implement REST endpoints for user operations

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  getProfile(@Req() req: any) {
+    return { user: req.user, message: 'Auth working!' };
+  }
+}
