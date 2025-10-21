4f008330bbd8d7269701b4d73c33125c86cb4ab7
Ephrata <ephrata0327@gmail.com>
2025-10-21 09:52:22 +0300

    change the redirecting url for production

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+const API_BASE_URL = 'https://taxitera.onrender.com'
 
 interface ApiError extends Error {
   status?: number;
